9007559221602662698ea755ebe6321b2226f029
lucky <dogralucky978@gmail.com>
2023-02-03 11:32:36 +0530

    test run

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const env = require("dotenv").config();
-
+var favicon = require("serve-favicon");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
-
+app.use(favicon(path.join(__dirname, "public/images", "favicon.ico")));
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
---
+++ controllers/userController.js
@@ -0,0 +1,4 @@
+exports.getSignUp = (req, res) => {
+  res.render("users/signUp");
+};
+const postSignUP = () => {};
